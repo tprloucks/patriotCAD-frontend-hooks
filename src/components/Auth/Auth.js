f65abdd93e6da45c0243e32819c2e856aa9310c3
Michael Loucks <michael.loucks@codeimmersives.com>
2021-08-18 14:06:44 -0500

    completed login page

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import ("./Auth.css")
 
 function Auth() {
     return (
-        <div>
-            Hello Login!
+        <>
+        <div className="background-wrap">
+            <div className="background"/>
         </div>
+        <form id="accesspanel" action="">
+            <h1 id="litheader">Patriot CAD Systems</h1>
+            <div className="inset">
+                <p>
+                    <input type="password" placeholder="Access Code" />
+                </p>
+                <p>
+                    <input type="text" placeholder="Badge # ex: B-00" />
+                </p>
+                <p className="p-container">
+                <input type="submit" name="Login" id="go"  defaultValue="Authorize" />
+          </p>
+            </div>
+        </form>
+        </>
     )
 }
 
